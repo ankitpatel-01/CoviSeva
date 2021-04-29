3444e359876dfdc4bf87f91e966d3d3c83ab6093
Ankit Patel <>
2021-04-29 12:23:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CoviSeva
+ Covid app where volunteers and help covid patients in home quranatime
---
+++ app/src/main/java/test/hexclan/coviseva/MainActivity.java
@@ -0,0 +1,14 @@
+package test.hexclan.coviseva;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
